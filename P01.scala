c12e0878ace764dab3932712f27e0a2cb887cfa8
lihanshang <lihanshang@hotmail.com>
2016-11-19 14:21:14 -0500

    Create P01.scala

---
+++ P01.scala
@@ -0,0 +1,17 @@
+/*
+P01 (*) Find the last element of a list.
+Example:
+scala> last(List(1, 1, 2, 3, 5, 8))
+res0: Int = 8
+*/
+object Solution {
+
+    def lastBuiltin(ls : List[Int]): Int = ls.last
+    def lastRecursive[A](ls : List[A]): A = ls match{
+        case h :: Nil  => h
+        case _ :: tail => lastRecursive(tail)
+        case _         => throw new NoSuchElementException
+    }
+    def main(args: Array[String]): Unit = {
+    }
+}
