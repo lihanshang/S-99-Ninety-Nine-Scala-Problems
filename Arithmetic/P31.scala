2c3b3c9a74f6d4f3a7fedcf5e4fa00f64c70fcf9
lihanshang <lihanshang@hotmail.com>
2016-12-06 18:46:07 -0500

    Update and rename P01.scala to P31.scala

---
+++ Arithmetic/P31.scala
@@ -0,0 +1,23 @@
+/*
+P31 (**) Determine whether a given integer number is prime.
+scala> 7.isPrime
+res0: Boolean = true
+*/
+
+object Solution {
+
+
+    def main(args: Array[String]): Unit = {
+
+        lazy val primes = 2 #:: Stream.from(3, 2).filter(isPrime)
+        def isPrime(n: Int): Boolean = primes.takeWhile(p => p * p <= n).forall(n % _ != 0)
+
+    }
+}
+/*
+def takeWhile(p: (A) ⇒ Boolean): Stream[A]
+Returns the longest prefix of this Stream whose elements satisfy the predicate p.
+
+def from(start: Int, step: Int): Stream[Int]
+Create an infinite stream starting at start and incrementing by step step.
+ */
