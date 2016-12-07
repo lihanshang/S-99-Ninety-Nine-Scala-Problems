1d20fc15e915a3acfc7401da5d73586c348d7835
lihanshang <lihanshang@hotmail.com>
2016-12-06 19:40:26 -0500

    Create P32.scala

---
+++ Arithmetic/P32.scala
@@ -0,0 +1,18 @@
+import java.lang
+import java.lang.System
+
+/*
+P32 (**) Determine the greatest common divisor of two positive integer numbers.
+Use Euclid's algorithm.
+scala> gcd(36, 63)
+res0: Int = 9
+*/
+
+object Solution {
+
+
+    def gcd(x: Int, y: Int): Int = {
+        if(y == 0) x else gcd(y, x % y)
+    }
+
+}
